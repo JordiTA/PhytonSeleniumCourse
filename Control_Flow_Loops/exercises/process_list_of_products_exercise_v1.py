fd31378ecefd6a7d19bf108c6590f175c84b7d0e
jtomasenti <jordi.tomas@enti.cat>
2023-06-26 22:03:41 +0200

    List of Products Exercise 1

---
+++ Control_Flow_Loops/exercises/process_list_of_products_exercise_v1.py
@@ -0,0 +1,58 @@
+"""
+Scenario:
+    You own an e-commerce marketplace site. Several partners sell their products on your site.
+    Like amazon.com. The partners update their list of products and prices daily. They send you the
+    product information in one of several ways (API, FTP, Email, ...).
+
+    You have received the products information and created a list as provided below.
+    Variable 'products' is the list of products you have received.
+"""
+
+products = [
+    {'id': 1, 'name': 't-shirt', 'price': 12.99, 'created_date': '2022-01-01', 'is_on_sale': False, 'sale_price': None},
+    {'id': 2, 'name': 'shoes', 'price': 22.45, 'created_date': '2022-01-01', 'is_on_sale': True, 'sale_price': None},
+    {'id': 3, 'name': 'dress-shirt', 'price': 43.00, 'created_date': '2022-01-01', 'is_on_sale': False, 'sale_price': None},
+    {'id': 4, 'name': 'socks', 'price': 14.99, 'created_date': '2022-01-01', 'is_on_sale': True, 'sale_price': 7.99},
+    {'id': 5, 'name': 'trousers', 'price': 32.50, 'created_date': '2022-01-01', 'is_on_sale': True, 'sale_price': None},
+    {'id': 6, 'name': 'jacket', 'price': 150.00, 'created_date': '2022-01-01', 'is_on_sale': False, 'sale_price': None},
+]
+
+print('-------------------------------------') # Separator
+"""
+Exercise 1:
+    - Iterate the products list and print the name of all products that have price greater than 25.
+"""
+
+print("Name of all the products that cost more than 25:")
+for i in products:
+    if i['price'] > 25:
+        print(i['name'])
+
+print('-------------------------------------') # Separator
+"""
+Exercise 2:
+    - Print the name and price of products that are on sale.
+"""
+print("Name of all the products that are ON SALE:")
+for i in products:
+    if i['is_on_sale']:
+        print(i['name'])
+
+print('-------------------------------------') # Separator
+"""
+Exercise 3:
+    - Create a list of products (product names) that are on sale but do not have a sale price.
+    - If a product is on sale but does not have sale price, you need to let the partner know. So the list of
+        products you generate will be shared with the partner. But for purpose of this exercise, just print the list
+        of sale products without sale price that you created.
+"""
+
+list = []
+
+for i in products:
+    if i['is_on_sale'] and i['sale_price'] == None:
+        list.append(i['name'])
+
+print(list)
+
+print('-------------------------------------') # Separator
