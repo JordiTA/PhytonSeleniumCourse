6953518839618a94fb4bac289ec9f6b736c1eeea
jtomasenti <jordi.tomas@enti.cat>
2023-07-03 11:00:58 +0200

    Reading & Writing Exercise Solved

--- Files/exercises/count_domains_in_email_list_file_exercise.py
+++ Files/exercises/count_domains_in_email_list_file_exercise.py
@@ -12,10 +12,53 @@ Example output:
 
 """
 
+import pdb
+import json
 
-input_file = "count_domains_in_email_list_file_exercise_input.csv"
-output_file = "count_domains_in_email_list_file_exercise_output.json"
+inputFile = "count_domains_in_email_list_file_exercise_input.csv"
+outputFile = "count_domains_in_email_list_file_exercise_output.json"
 
 
+# # --------------------------------------------------------
 
+def readingEmailsFromFile(filePath):
+    print(f"Reading file in: {filePath}")
+    with open(filePath, 'r') as file:
+        content = file.readlines()
 
+    contentClean = [i.strip(',\n') for i in content]
+
+    return contentClean
+
+# # --------------------------------------------------------
+
+def countDomains(emailsList):
+    print("Counting Domains")
+    domainCount = {}
+    for email in emailsList:
+        domain = email.split('@')[-1]
+    
+        if domain not in domainCount.keys():
+            domainCount[domain] = 1
+        else:
+            domainCount[domain] = domainCount[domain] + 1
+    
+    return domainCount
+
+# # --------------------------------------------------------
+
+def saveOutputFile(countsDict, jsonFilePath):
+    print(f"Wrinting json File output in: {jsonFilePath}")
+    with open(jsonFilePath, 'w') as file:
+        jsonWrite = json.dumps(countsDict, indent=4)
+        file.write(jsonWrite)
+
+# # --------------------------------------------------------
+# # Calling Functions
+
+# READING
+domainsList = readingEmailsFromFile(inputFile)
+# FORMATING
+domainDict = countDomains(domainsList)
+# WRITING
+saveOutputFile(domainDict, outputFile)
