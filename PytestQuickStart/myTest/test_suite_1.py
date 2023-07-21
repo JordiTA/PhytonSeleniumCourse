cd7af033a1c68809b468f2d5f0a7ed9bac6fe6bd
jtomasenti <jordi.tomas@enti.cat>
2023-07-21 17:22:05 +0200

    Pytest Marks

--- PytestQuickStart/myTest/test_suite_1.py
+++ PytestQuickStart/myTest/test_suite_1.py
@@ -1,7 +1,16 @@
+import pytest
 
+pytestmark = [pytest.mark.frontend, pytest.mark.slow]
+
+@pytest.mark.smoke
 def testLoginPageValidUser():
     print("")
     print("Login with valid User.")
 
+@pytest.mark.regression
+@pytest.mark.smoke
 def testLoginWrongPassword():
     print("Login with wrong Password.")
+
+# RUN ONE IN TERMINAL --> pytest -m smoke --> Runs the two functions
+# RUN ONE WITH TWO TAGS IN TERMINAL --> pytest -m "smoke and regression" --> Runs only the second function
