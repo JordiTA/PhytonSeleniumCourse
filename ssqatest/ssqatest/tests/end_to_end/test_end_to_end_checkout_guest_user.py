7a661968b5f364e45b4004ba21bceceb4e127fe2
jtomasenti <jordi.tomas@enti.cat>
2023-07-24 22:52:20 +0200

    TCID33 Part 1: Organized Overview

---
+++ ssqatest/ssqatest/tests/end_to_end/test_end_to_end_checkout_guest_user.py
@@ -0,0 +1,27 @@
+import pytest
+
+@pytest.mark.usefixtures('init_driver')
+class TestEndToEndCheckoutGuestUser:
+
+    def testEndToEndCheckoutGuestUser(self):
+
+        # GO TO HOME PAGE
+
+        # ADD 1 ITEM TO CARD
+
+        # GO TO CART
+
+        # APPLY FREE CUPON
+
+        # SLECT FREE SHIPPING
+
+        # CLICK ON CHECKOUT
+
+        # FILL INFORM
+
+        # CLICK ON PLACE ORDER
+
+        # VERIFY ORDER IS RECIEVED
+
+        # VERYFY ORDER IS RECORDED IN DATABASE (via SQL // via API)
+        pass
